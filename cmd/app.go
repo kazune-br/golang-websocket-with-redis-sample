59769c9b58c96a64d1a0218a89dcffcd6057ae8f
kazune <kazune.miyagi92@gmail.com>
2021-09-27 11:57:09 +0900

    remove reduandant comment

--- cmd/app.go
+++ cmd/app.go
@@ -7,36 +7,5 @@ import (
 
 func main() {
 	logging.InitLogger()
-	//var ctx = context.Background()
-	//for {
-	//	// Publish a generated user to the new_users channel
-	//	err := conn.Client.Publish(ctx,
-	//		"new_users",
-	//		struct {
-	//			value string
-	//		}{
-	//			value: "Hello",
-	//		},
-	//	).Err()
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	// Sleep random time
-	//	rand.Seed(time.Now().UnixNano())
-	//	n := rand.Intn(4)
-	//	time.Sleep(time.Duration(n) * time.Second)
-	//}
-
-	//err := conn.Client.Set(ctx, "key", "value", 0).Err()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//val, err := conn.Client.Get(ctx, "key").Result()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("key", val)
-	//defer conn.Client.Close()
 	router.Run()
 }
